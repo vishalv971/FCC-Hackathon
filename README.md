2dbb325593d64c83bbbcacc13d134112c253dc36
Vishal Vishwanathan <vishalviswanathan9@gmail.com>
2017-12-13 19:26:23 +0530

    Updated the remaining files

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# FCC-Hackathon
-A social networking site for FCC-Bengaluru
+<p align="center"><img width="40%" src="https://design-style-guide.freecodecamp.org/downloads/freeCodeCamp.jpg" /></p>
+
+--------------------------------------------------------------------------------
+
+### Introduction
+This repository contains the website for Free Code Camp Bengaluru Group. (https://www.facebook.com/groups/free.code.camp.bengaluru/)
+
+--------------------------------------------------------------------------------
+
+###  Pre-requisites to run demo
+
+- node.js
+- npm
+- express
+- ejs
+- body-parser
+- client-sessions
+- serve-favicon
+
+### Demo
+```
+git clone https://github.com/ankushop2/FCC-Hackathon.git
+
+cd FCC-Hackathon
+
+npm init
+sudo npm install express --save
+sudo npm install ejs --save
+sudo npm install body-parser --save
+sudo npm install client-sessions --save
+sudo npm install serve-favicon --save
+
+node app.js
+```
+
+Open browser and navigate to (http://localhost:3000)
+
+--------------------------------------------------------------------------------
+
+### Description
+ In the following project we have 3 modules incorporated along with [Firebase](https://firebase.google.com/) auth. We've used the node client-session modules to perform session handling using cookies. The front-end has been designed using [materializecss](http://materializecss.com) and [particles.js](https://github.com/VincentGarreau/particles.js/)
+ 
+ --------------------------------------------------------------------------------
+ 
+ ### Additional Features
+ 
+ - Search Engine Optimization
+ - Adding and Deleting events
+ - Social Sign-in using Google
+ 
+ --------------------------------------------------------------------------------
+### Screenshots
+<p align="center"><img width="80%" src="/screenshots/login.jpg" /></p>
+<p align="center"><img width="80%" src="/screenshots/loginwithgoogle.jpg" /></p>
+<p align="center"><img width="80%" src="/screenshots/register.jpg" /></p>
+<p align="center"><img width="80%" src="/screenshots/home.jpg" /></p>
+<p align="center"><img width="80%" src="/screenshots/dashboard.jpg" /></p>
+<p align="center"><img width="80%" src="/screenshots/events.jpg" /></p>
+<p align="center"><img width="80%" src="/screenshots/new.jpg" /></p>
+<p align="center"><img width="80%" src="/screenshots/about.jpg" /></p>
+<p align="center"><img width="80%" src="/screenshots/footer.jpg" /></p>
+ 
+ --------------------------------------------------------------------------------
---
+++ app.js
@@ -0,0 +1,145 @@
+var firebase = require("firebase");
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
+var session = require("client-sessions");
+var favicon = require('serve-favicon');
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(favicon(__dirname + '/public/img/glyph.png'));
+
+
+// FIREBASE CONFIG
+var config = {
+	apiKey: "AIzaSyCufIc48jvfrtzuichCuQ1TkrfWaF_jzs8",
+	authDomain: "fcc-hack.firebaseapp.com",
+	databaseURL: "https://fcc-hack.firebaseio.com/",
+	projectId: "fcc-hack",
+	storageBucket: "fcc-hack.appspot.com",
+};
+
+firebase.initializeApp(config);
+var ref = firebase.database().ref();
+
+app.use(session({
+  	cookieName: 'session',
+  	secret: 'kjhkjajdhd89whd',
+  	duration: 30 * 60 * 1000,
+  	activeDuration: 5 * 60 * 1000,
+  	httpOnly: true,
+	secure: true,
+	ephemeral: true
+}));
+
+
+// ROUTES
+app.get("/", function(req, res) {
+    res.redirect("/login");
+});
+
+app.get("/login", function(req, res) {
+	res.render("login");
+});
+
+app.post("/login",function(req,res){
+		var email = req.body.email;
+		var password = req.body.password;
+
+		firebase.auth().signInWithEmailAndPassword(email, password).then(function(user) {
+			req.session.user = user;
+			res.redirect("/home");
+		}).catch(function(error) {
+			var errorCode = error.code;
+			var errorMessage = error.message;
+
+			if (errorCode === 'auth/wrong-password') {
+				console.log('Wrong password...');
+			} else {
+				console.log(errorMessage);
+			}
+			console.log(error);
+		});
+});
+
+app.post("/loginWithGoogle", function(req, res) {
+	req.session.user = req.body.user;
+	res.send("/home");
+});
+
+app.post("/register", function(req, res) {
+	res.render("register");
+});
+
+app.get("/home", function(req, res) {
+	if(!req.session || !req.session.user)
+		res.redirect("/login");
+	else
+    res.render("home");
+});
+
+app.get("/events", function(req, res) {
+	if(!req.session || !req.session.user)
+		res.redirect("/login");
+
+	else {
+		ref.once("value").then(function(snap) {
+			var json = snap.val();
+			var arr = [];
+
+			for(var i in json)
+			  arr.push( json[i] );
+
+			res.render("events", {events: arr, removeKey: null});
+		});
+	}
+});
+
+app.post("/events", function(req, res) {
+	var event = ref.push();
+	event.set({
+		name: req.body.name,
+		description: req.body.description,
+	});
+
+	res.redirect("/events");
+});
+
+app.get("/new", function(req, res) {
+	if(!req.session || !req.session.user)
+		res.redirect("/login");
+	else
+		res.render("newEvent");
+});
+
+app.get("/dashboard", function(req, res) {
+	if(!req.session || !req.session.user)
+		res.redirect("/login");
+	else
+		res.render("dashboard");
+});
+
+app.get("/about", function(req, res) {
+	if(!req.session || !req.session.user)
+		res.redirect("/login");
+	else
+		res.render("about");
+});
+
+app.get("/logout", function(req, res) {
+	firebase.auth().signOut().then(function() {
+		req.session.reset();
+		res.redirect("/login");
+	},function(error) {
+		  console.error('Sign Out Error', error);
+	});
+});
+
+
+// SERVER STARTUP
+app.listen(3000, "localhost", function() {
+    console.log("Server started on port number 3000...");
+});
